1b0563906a09c9e212f281cd407af43567d4f3f7
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-07-28 02:47:15 +0600

    deployed

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwCWWHKxzrBHu5n1Oa4w2ovFl5WX603tM",
+  authDomain: "futuristic-technologies.firebaseapp.com",
+  projectId: "futuristic-technologies",
+  storageBucket: "futuristic-technologies.appspot.com",
+  messagingSenderId: "63418647140",
+  appId: "1:63418647140:web:69f1265f2b4bd9f5a01571"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
